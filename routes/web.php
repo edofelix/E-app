8667fe6ad1d9d39681e9a14a52d30373300418ef
edofelix <edofelix9927@gmail.com>
2021-04-21 22:14:02 +0700

    update route untuk fungsi tambah

--- routes/web.php
+++ routes/web.php
@@ -8,12 +8,11 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/test/{var}', function ($var) {
-    return "$var";
-});
+
 
 Route::get('/home', [Econtroller::class, 'home']);
 Route::get('/ehome', [Econtroller::class, 'ehome'])->name('ehome');
 Route::get('/egaleri', [Econtroller::class, 'egaleri']);
 
 Route::post('/ehome/update', [Econtroller::class, 'update']);
+Route::post('/ehome/tambah', [Econtroller::class, 'tambah']);
